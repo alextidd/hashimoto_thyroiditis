fb8ada869c778e7b39a7c12b1754bcd35ee1137d
alextidd <at31@sanger.ac.uk>
2024-11-25 15:24:24 +0000

    update shapeit resources

--- src/04_run_PTATO.sh
+++ src/04_run_PTATO.sh
@@ -10,12 +10,16 @@ module load singularity
 module load ISG/rocker/rver/4.4.0 
 export R_LIBS_USER=$HOME/R-tmp-4.4
 nextflow run $wd/../PTATO/ptato.nf \
-  -c $wd/config/run.config \
+  --run.svs false \
+  --run.cnvs false \
+  --shapeit.reference /lustre/scratch125/casm/team268im/at31/PTATO/resources/hg38/shapeit/Phasing_reference_no_chr/ \
+  --shapeit.maps /lustre/scratch125/casm/team268im/at31/PTATO/resources/hg38/shapeit/shapeit_maps_no_chr/ \
   -c ~/.nextflow/config \
   -c $wd/../PTATO/configs/run-template.config \
   -c $wd/../PTATO/configs/nextflow.config \
   -c $wd/../PTATO/configs/process.config \
   -c $wd/../PTATO/configs/resources.config \
+  -c $wd/config/run.config \
   --out_dir ./ \
   -w $wd/work/ptato/ \
   -resume \
