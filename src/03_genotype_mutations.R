0cc664d6370303c003d43cb5d29fb309a7c35a4d
alextidd <at31@sanger.ac.uk>
2024-11-25 15:24:45 +0000

    genotype PD63118 mutations in PTA

--- src/03_genotype_mutations.R
+++ src/03_genotype_mutations.R
@@ -1,5 +1,14 @@
+#!/usr/bin/env Rscript
+# cd /lustre/scratch125/casm/team268im/at31/resolveome ; bsub -q long -M20000 -R 'span[hosts=1] select[mem>20000] rusage[mem=20000]' -J genotype_pta -o log/genotype_pta_%J.out -e log/genotype_pta_%J.err 'module load ISG/rocker/rver/4.4.0; export R_LIBS_USER=$HOME/R-tmp-4.4; Rscript src/03_genotype_mutations.R'
+
 # libraries
 library(magrittr)
+
+# dirs
+dir.create("out/genotyping/")
+
+# id of interest (mutations in the florid case)
+curr_pdid <- "PD63118"
 
 # get mutations from exome and targeted nanoseq
 nanoseq_dir <- "/lustre/scratch126/casm/team268im/al28/targeted_nanoseq/"
@@ -13,53 +22,48 @@ nanoseq_muts <-
       dplyr::mutate(pdid = stringr::str_sub(sampleID, 1, 7))
   }) %>%
   dplyr::bind_rows(.id = "seq_type") %>%
-  dplyr::distinct(pdid, sampleID, chr, pos, ref, mut, seq_type, times_called)
-
-# get mutations in the florid case
-florid_muts <-
-  nanoseq_muts %>%
-  dplyr::filter(pdid == "PD63118")
+  dplyr::filter(pdid == curr_pdid)
   
-# run dndscv to get annotate mutations
-dndsout <- dndscv::dndscv(florid_muts)
-sel_cv <- dndsout$sel_cv
-print(head(sel_cv), digits = 3)
-signif_genes <- sel_cv[sel_cv$qglobal_cv < 0.1, c("gene_name","qglobal_cv")]
-rownames(signif_genes) <- NULL
-print(signif_genes)
+# run dndscv to annotate mutations
+dndsout <-
+  nanoseq_muts %>%
+  dplyr::distinct(sampleID, chr, pos, ref, mut) %>%
+  dndscv::dndscv()
+nanoseq_muts <-
+  nanoseq_muts %>%
+  dplyr::left_join(dndsout$annotmuts, relationship = "many-to-many")
+
+# we only want to look at mutations that are in the exome
+gene_muts <-
+  nanoseq_muts %>%
+  dplyr::filter(!is.na(gene))
 
 # genotype these sites in the PTA bams
 ss <- readr::read_csv("out/caveman/samplesheet.csv")
-annotmuts <-
-  dplyr::left_join(florid_muts, dndsout$annotmuts) %>%
-  dplyr::arrange(-times_called) %>%
-  dplyr::filter(!is.na(gene)) %>%
-  dplyr::select(pdid, chr, pos, ref, mut, gene, times_called)
 
-# genotype the site
-bam <- ss[1, ]$tumour_bam
-site <- annotmuts[1, ]
-
+# genotype all sites
 pta_muts <-
-  annotmuts %>%
-  dplyr::filter(nchar(ref) == 1, nchar(mut) == 1) %>%
-  purrr::pmap(function(pdid, chr, pos, ref, mut, gene, times_called) {
+  gene_muts %>%
+  dplyr::select(chr, pos, ref, mut, gene) %>%
+  purrr::pmap(function(chr, pos, ref, mut, gene) {
+    paste(chr, pos, ref, mut, gene, "\n") %>% cat()
+    ss %>%
+      dplyr::filter()
     split(ss$tumour_bam, ss$sample_id) %>%
       purrr::map(function(bam) {
-        calls <- deepSNV::bam2R(bam, chr, pos, pos)
+        calls <- deepSNV::bam2R(bam, chr, pos, pos,
+                                q = 30, mask = 3844, mq = 30)
+        total_depth <- sum(calls[, c("A", "C", "G", "T", "a", "c", "g", "t",
+                                     "DEL", "INS", "del", "ins")], na.rm = TRUE)
+        mut_depth <- sum(calls[, c(mut, tolower(mut))], na.rm = TRUE)
         tibble::tibble(chr = chr, pos = pos, ref = ref, mut = mut, gene = gene,
-                      times_called = times_called, calls = calls) %>%
-          dplyr::mutate(n_ref = calls[, ref] + calls[, tolower(ref)],
-                        n_mut = calls[, mut] + calls[, tolower(mut)])
+                       total_depth = total_depth, mut_depth = mut_depth)
       }) %>%
       dplyr::bind_rows(.id = "sample_id")
   }) %>%
-  dplyr::bind_rows()
+  dplyr::bind_rows() %>%
+  dplyr::left_join(nanoseq_muts)
 
+# write mut calls to out
 pta_muts %>%
-  dplyr::mutate(vaf = n_mut / (n_ref + n_mut)) %>%
-  dplyr::select(vaf, n_ref, n_mut, everything()) %>%
-  dplyr::arrange(-vaf) %>%
-  dplyr::filter(vaf > 0) %>%
-  dplyr::count(chr, pos, ref, mut, gene) %>%
-  dplyr::arrange(-n)
+  readr::write_tsv("out/genotyping/pta_muts.tsv")
