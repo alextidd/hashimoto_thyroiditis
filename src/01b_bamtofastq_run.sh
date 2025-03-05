6534529d78bb7d2bf0da1a5f2b7abc9c1b3ec4fd
alextidd <at31@sanger.ac.uk>
2025-03-05 12:08:37 +0000

    begin analysing bj-somatic-variantcalling wgs output, allow retry on failling processes

--- src/01b_bamtofastq_run.sh
+++ src/01b_bamtofastq_run.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-# cd /lustre/scratch125/casm/team268im/at31/resolveome ; bsub -q basement -M2000 -R 'span[hosts=1] select[mem>2000] rusage[mem=2000]' -J 01d_bamtofastq_run -o log/%J_01d_bamtofastq_run.out -e log/%J_01d_bamtofastq_run.err 'bash src/01d_bamtofastq_run.sh'
+# cd /lustre/scratch125/casm/team268im/at31/resolveome ; bsub -q basement -M2000 -R 'span[hosts=1] select[mem>2000] rusage[mem=2000]' -J 01b_bamtofastq_run -o log/%J_01b_bamtofastq_run.out -e log/%J_01b_bamtofastq_run.err 'bash src/01b_bamtofastq_run.sh'
 
 # dirs
 wd=$(pwd)
