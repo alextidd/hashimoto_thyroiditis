1364dacd1dd7da28558a7d334e3828a657ee19db
alextidd <at31@sanger.ac.uk>
2025-03-29 17:28:33 +0000

    run differential expression analysis, run bj-expression on filtered cells, analyse filtered dna output, lower sentieon resources for dnahyb alignment

--- src/04i_BaseJumper_expression_filter_cells_run.sh
+++ src/04i_BaseJumper_expression_filter_cells_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# cd /lustre/scratch125/casm/team268im/at31/resolveome ; bsub -q basement -M10000 -R 'span[hosts=1] select[mem>10000] rusage[mem=10000]' -J 04e_BaseJumper_expression_run -o log/%J_04e_BaseJumper_expression_run.out -e log/%J_04e_BaseJumper_expression_run.err 'bash src/04e_BaseJumper_expression_run.sh'
+# cd /lustre/scratch125/casm/team268im/at31/resolveome ; bsub -q basement -M2000 -R 'span[hosts=1] select[mem>2000] rusage[mem=2000]' -J 04i_BaseJumper_expression_filter_cells_run -o log/%J_04i_BaseJumper_expression_filter_cells_run.out -e log/%J_04i_BaseJumper_expression_filter_cells_run.err 'bash src/04i_BaseJumper_expression_filter_cells_run.sh'
 
 # dirs
 wd=$(pwd)
@@ -13,14 +13,14 @@ export LSB_EXCLUSIVE=Y
 
 # run bj-expression
 (
-  cd out/BaseJumper/bj-expression/all_cells/
+  cd out/BaseJumper/bj-expression/filter_cells/
   nextflow run $wd/../nextflow/external/BaseJumper/bj-expression \
     --input_csv samplesheet.csv \
     --publish_dir PD63118 \
     --genome GRCh38 \
     --skip_subsampling \
     --tmp_dir $TMPDIR \
-    -w $wd/work/BaseJumper/bj-expression/all_cells/ \
+    -w $wd/work/BaseJumper/bj-expression/filter_cells/ \
     -c $wd/config/basejumper.config \
     -profile singularity \
     -resume
